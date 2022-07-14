9e8af4ec69dfe6f9f55daaf47cbbd70149a011f2
heewon916 <kimhiwon@naver.com>
2022-07-13 20:03:52 -0700

    move heuristic function move to lab2.py and make class a_star to manage the g_cnt, h_cnt, total_f variable

--- lab2.py
+++ lab2.py
@@ -1,17 +1,47 @@
 # you can add imports but you should not rely on libraries that are not already provided in "requirements.txt #
 from heapq import heappush, heappop
 
+class a_star():
+    def __init__(self):
+        self.g_cnt = 0
+        self.h_cnt = 0
+        self.total_f = self.g_cnt + self.h_cnt
+        
+    def heuristic(self): 
+        # should check 4 conditions on the pair of stacks
+        # so we should use self.order, self.orientations
+        for idx, front_matter in enumerate(self.orientations, step=2):
+            self.next = idx + 1
+            if (self.order[idx] > self.order[self.next]):
+                h_cnt += 1
+            if ((self.order[idx] > self.order[self.next]) and 
+                (self.orientations[self.next] == 1 and front_matter == 1)):
+                h_cnt += 1
+            if ((self.orientations[self.next],front_matter) == (0,1)
+                or (self.orientations[self.next],front_matter) == (1,0)):
+                h_cnt += 1
+            if ((self.order[idx] < self.order[self.next])
+                and (self.orientations[self.next],front_matter) == (0,0)):
+                h_cnt += 1         
 
+    ##--edit: to calc the g_cnt; the num of the flip count 
+    def g_func(self):
+        g_cnt += 1
+    
 def a_star_search(stack):
+    # flip할 위치를 a* search 로 찾으려고 하는 거니까 
+    # stack의 내용을 heap으로 구현한 다음에
+    # 거기서 최소 f(=g+h)값을 찾는, 즉 루트 값을 찾아오면 됨
     # INPUT 
     ## TextbookStack(initial_order=[3, 2, 1, 0], initial_orientations=[0, 0, 0, 0])
     # OUTPUT
     ## "a_star_search": [4]
     flip_sequence = []
-    
 
     # --- v ADD YOUR CODE HERE v --- #
-    
+    # for matter in stack.order:
+    #     heappush(matter)
+    #     stack.
     # where we will flip; the sequence
     return flip_sequence
 
